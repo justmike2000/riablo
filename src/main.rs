9d685cb56f1779d3f4bb19acfb75db1f685ce4fe
Michael Mileusnich <michaelmileusnich@Michaels-MacBook-Air.local>
2022-02-27 11:08:50 -0600

    More

---
+++ src/main.rs
@@ -0,0 +1,104 @@
+
+use oorandom::Rand32;
+
+use ggez::event::{KeyCode, KeyMods};
+use ggez::{event, graphics, timer, Context, GameResult};
+
+const GRID_SIZE: (i16, i16) = (30, 20);
+const GRID_CELL_SIZE: (i16, i16) = (32, 32);
+
+const SCREEN_SIZE: (f32, f32) = (
+    GRID_SIZE.0 as f32 * GRID_CELL_SIZE.0 as f32,
+    GRID_SIZE.1 as f32 * GRID_CELL_SIZE.1 as f32,
+);
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
+impl Direction {
+    pub fn inverse(&self) -> Self {
+        match *self {
+            Direction::Up => Direction::Down,
+            Direction::Down => Direction::Up,
+            Direction::Left => Direction::Right,
+            Direction::Right => Direction::Left,
+        }
+    }
+
+    pub fn from_keycode(key: KeyCode) -> Option<Direction> {
+        match key {
+            KeyCode::Up => Some(Direction::Up),
+            KeyCode::Down => Some(Direction::Down),
+            KeyCode::Left => Some(Direction::Left),
+            KeyCode::Right => Some(Direction::Right),
+            _ => None,
+        }
+    }
+}
+
+struct Player {
+}
+
+impl Player {
+
+    fn update(&mut self) {
+    }
+
+    fn draw(&self, ctx: &mut Context) -> GameResult<()> {
+        Ok(())
+    }
+}
+
+struct GameState {
+    player: Player,
+}
+
+impl GameState {
+    pub fn new() -> Self {
+
+        GameState {
+            player: Player {}
+        }
+    }
+}
+
+impl event::EventHandler<ggez::GameError> for GameState {
+    fn update(&mut self, ctx: &mut Context) -> GameResult {
+        Ok(())
+    }
+
+    fn draw(&mut self, ctx: &mut Context) -> GameResult {
+        graphics::clear(ctx, [0.0, 1.0, 0.0, 1.0].into());
+        self.player.draw(ctx)?;
+        graphics::present(ctx)?;
+        ggez::timer::yield_now();
+        Ok(())
+    }
+
+    fn key_down_event(
+        &mut self,
+        _ctx: &mut Context,
+        keycode: KeyCode,
+        _keymod: KeyMods,
+        _repeat: bool,
+    ) {
+    }
+}
+
+fn main() -> GameResult {
+
+    let window_setup = ggez::conf::WindowSetup::default().title("Riablo");
+
+    let (ctx, events_loop) = ggez::ContextBuilder::new("player", "Mitt Miles")
+        .window_setup(window_setup)
+        .window_mode(ggez::conf::WindowMode::default().dimensions(SCREEN_SIZE.0, SCREEN_SIZE.1))
+        .build()?;
+
+    let state = GameState::new();
+    event::run(ctx, events_loop, state)
+}
