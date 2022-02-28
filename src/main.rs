3066ba5384e4dc834c8648206a819202f54b6c30
Michael Mileusnich <michaelmileusnich@Michaels-MacBook-Air.local>
2022-02-27 22:45:16 -0600

    Working texture

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,17 @@
 
 use oorandom::Rand32;
 
+use glam::*;
+use ggez::{event, timer, Context, GameResult, graphics};
 use ggez::event::{KeyCode, KeyMods};
-use ggez::{event, graphics, timer, Context, GameResult};
+use ggez::graphics::{GlBackendSpec, Image, draw,
+                     ImageGeneric, clear, present};
 
-const GRID_SIZE: (i16, i16) = (30, 20);
-const GRID_CELL_SIZE: (i16, i16) = (32, 32);
+use std::path;
+use std::env;
 
-const SCREEN_SIZE: (f32, f32) = (
-    GRID_SIZE.0 as f32 * GRID_CELL_SIZE.0 as f32,
-    GRID_SIZE.1 as f32 * GRID_CELL_SIZE.1 as f32,
-);
+const RESOLUTION: (f32, f32) = (800.0, 600.0);
+
 
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
 enum Direction {
@@ -42,14 +43,29 @@ impl Direction {
 }
 
 struct Player {
+    texture: ImageGeneric<GlBackendSpec>,
 }
 
 impl Player {
+
+    fn new(ctx: &mut Context) -> Player {
+        Player {
+            texture: Image::new(ctx, 
+                "/hero.png".to_string()).unwrap(),
+        }
+    }
 
     fn update(&mut self) {
     }
 
     fn draw(&self, ctx: &mut Context) -> GameResult<()> {
+
+        let param = graphics::DrawParam::new()
+        .src(graphics::Rect {x: 0.00, y: 0.00, w: 0.25, h: 0.25})
+        .dest(Vec2::new(20.00, 20.0));
+        //.offset(Vec2::new(0.15, self.jump_offset))
+        //.scale(Vec2::new(0.1, 0.1));
+        draw(ctx, &self.texture, param)?;
         Ok(())
     }
 }
@@ -59,10 +75,10 @@ struct GameState {
 }
 
 impl GameState {
-    pub fn new() -> Self {
+    pub fn new(ctx: &mut Context) -> Self {
 
         GameState {
-            player: Player {}
+            player: Player::new(ctx)
         }
     }
 }
@@ -73,9 +89,9 @@ impl event::EventHandler<ggez::GameError> for GameState {
     }
 
     fn draw(&mut self, ctx: &mut Context) -> GameResult {
-        graphics::clear(ctx, [0.0, 1.0, 0.0, 1.0].into());
+        clear(ctx, [0.0, 1.0, 0.0, 1.0].into());
         self.player.draw(ctx)?;
-        graphics::present(ctx)?;
+        present(ctx)?;
         ggez::timer::yield_now();
         Ok(())
     }
@@ -94,11 +110,20 @@ fn main() -> GameResult {
 
     let window_setup = ggez::conf::WindowSetup::default().title("Riablo");
 
-    let (ctx, events_loop) = ggez::ContextBuilder::new("player", "Mitt Miles")
+    let resource_dir = if let Ok(manifest_dir) = env::var("CARGO_MANIFEST_DIR") {
+        let mut path = path::PathBuf::from(manifest_dir);
+        path.push("textures");
+        path
+    } else {
+        path::PathBuf::from("./textures")
+    };
+
+    let (mut ctx, events_loop) = ggez::ContextBuilder::new("player", "Mitt Miles")
         .window_setup(window_setup)
-        .window_mode(ggez::conf::WindowMode::default().dimensions(SCREEN_SIZE.0, SCREEN_SIZE.1))
+        .window_mode(ggez::conf::WindowMode::default().dimensions(RESOLUTION.0, RESOLUTION.1))
+        .add_resource_path(resource_dir)
         .build()?;
 
-    let state = GameState::new();
+    let state = GameState::new(&mut ctx);
     event::run(ctx, events_loop, state)
 }
